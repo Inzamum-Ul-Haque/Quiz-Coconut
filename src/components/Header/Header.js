859bf38452c7ad60989f051d6f5a8cb5f1ffb551
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-10 22:58:23 +0600

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            React Bootstrap
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
